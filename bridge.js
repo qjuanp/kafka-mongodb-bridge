4997a795dbfaf4e827d2006c8939d56ace13b9bf
Juan P. Giraldo <qjuanp@gmail.com>
2016-04-03 11:29:19 -0500

    Kafka consumer

---
+++ bridge.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var kafka = require('kafka-node');
+var Consumer = kafka.Consumer;
+var Offset = kafka.Offset;
+var Client = kafka.Client;
+
+var topic = 'temperature';
+
+var client = new Client('172.24.98.29:8080');
+var topics = [
+        {topic: topic, partition: 0}
+    ],
+    options = { autoCommit: true, fetchMaxWaitMs: 1000, fetchMaxBytes: 1024*1024 };
+
+var consumer = new Consumer(client, topics, options);
+var offset = new Offset(client);
+
+consumer.on('message', function (message) {
+    console.log(message.value);
+});
+
+consumer.on('error', function (err) {
+    console.log('error', err);
+});
+
+console.log('all up');
+
+/*
+* If consumer get `offsetOutOfRange` event, fetch data from the smallest(oldest) offset
+*/
+consumer.on('offsetOutOfRange', function (topic) {
+    topic.maxNum = 2;
+    offset.fetch([topic], function (err, offsets) {
+        var min = Math.min.apply(null, offsets[topic.topic][topic.partition]);
+        consumer.setOffset(topic.topic, topic.partition, min);
+    });
+});
